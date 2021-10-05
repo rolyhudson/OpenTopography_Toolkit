83972adc2c356b8b300a5a986d6ae8706dbb9f45
rolyh <roly.hudson@gmail.com>
2021-10-05 13:37:51 +0100

    Setup initial pull

---
+++ OpenTopology_oM/GlobalDEMRequest.cs
@@ -0,0 +1,16 @@
+﻿using BH.oM.Base;
+using BH.oM.Data.Requests;
+using BH.oM.Geospatial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Adapter.OpenTopology
+{
+    public class GlobalDEMRequest : BHoMObject, IRequest
+    {
+        public virtual BoundingBox BoundingBox { get; set; } = new BoundingBox();
+    }
+}
---
+++ OpenTopology_oM/OpenTopologyConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Adapter;
+
+namespace BH.oM.Adapter.OpenTopology
+{
+    [Description("Define configuration settings for pulling OpenTopology data using the OpenTopology Adapter")]
+    public class OpenTopologyConfig : ActionConfig
+    {
+        public virtual DemType DemType { get; set; } = DemType.AW3D30;
+    }
+}
